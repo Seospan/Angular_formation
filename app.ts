578f5b464a974759138fef775d0202f0a80ef37b
Seospan <sophie.jobez@gmail.com>
2016-05-31 13:57:24 +0200

    Angular

--- app.ts
+++ app.ts
@@ -38,15 +38,67 @@ class ArticleComponent {
   }
 }
 */
+
+@Component({
+  selector: 'products-list',
+  //inputs: ['article'],
+  template: `
+    `
+})
+class ProductsList {
+  //article: Article;
+
+ /* voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }*/
+}
+
+
 @Component({
   selector: 'inventory-app',
-  //directives: [ArticleComponent],
+  directives: [ProductsList],
   template: `<div class="inventory-app">
+            <products-list [productList]="products" (onProductSelected)="productWasSelected($event)">
+            
+            </products-list>
+            <div *ngFor="let product of products">
+                            <h1>{{ product.name }}</h1>
+            <span>{{ product.sku }}</span>
+            </div>
+
                 (Products will go here soon)
         </div>
   `
 })
 class InventoryApp {
+
+  products: Product[];
+  
+  constructor() {
+     this.products = [
+       new Product(
+         'MYSHOES', 'Black Running Shoes',
+         '/resources/images/products/black-shoes.jpg',
+         ['Men', 'Shoes', 'Running Shoes'],
+         109.99),
+      new Product(
+         'NEATOJACKET', 'Blue Jacket',
+         '/resources/images/products/blue-jacket.jpg',
+         ['Women', 'Apparel', 'Jackets & Vests'],
+         238.99),
+       new Product(
+         'NICEHAT', 'A Nice Black Hat',
+         '/resources/images/products/black-hat.jpg',
+         ['Men', 'Accessories', 'Hats'],
+         29.99)
+       ];
+  }
+
+    productWasSelected(product : Product):void{
+        console.log('Product clicked: ', product)
+    }
+
   /*articles: Article[];
 
   constructor() {
